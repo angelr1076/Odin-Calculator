74943bbfd2aaef1b3252edf6630800137202942b
angelr1076 <angelr1076@gmail.com>
2022-09-08 08:32:01 -0400

    Completed styling and updated HTML operators

--- scripts/main.js
+++ scripts/main.js
@@ -1,86 +1,154 @@
-const buttons = document.querySelector('.buttons');
-const buttonNodeList = buttons.querySelectorAll('.button');
-const display = document.querySelector('.display');
-let stack = []; // Operands array
-let operators = []; // Operators array
-let subStack = [];
+const currentDisplay = document.querySelector('[data-current]');
+const previousDisplay = document.querySelector('[data-previous]');
+const equals = document.querySelector('[data-equal');
+const clear = document.querySelector('[data-all-clear]');
+const deleteCurrent = document.querySelector('[data-delete]');
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operator]');
+const decimal = document.querySelector('[data-decimal]');
+let currentNum = '';
+let previousNum = '';
+let operator = '';
 
-function multiply(a, b) {
-  return a * b;
-}
+function operate() {
+  previousNum = Number(previousNum);
+  currentNum = Number(currentNum);
 
-function divide(a, b) {
-  return a / b;
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-// Check if the value is a number or an operand
-function isOperator(value) {
-  const operatorsObject = {
-    multiply: '*',
-    divide: '/',
-    add: '+',
-    subtract: '-',
-  };
-
-  for (let key in operatorsObject) {
-    if (value === operatorsObject[key]) return true;
-  }
-  return false;
-}
-
-// Create the functions that populate the display when you click the number buttons.
-function renderDisplay(entry) {
-  const num = storeInput(entry);
-  display.textContent = num;
-}
-
-// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
-function storeInput(val) {
-  for (let i = 0; i < val.length; i++) {
-    let value = val[i];
-    if (isOperator(value)) {
-      operators.push(value);
-    } else if (!isOperator(value) && operators.length > 0) {
-      subStack.push(value);
-    } else {
-      stack.push(value);
-    }
-
-    if (val === '=') {
-      return operate(operators[0], stack.join(''), subStack.join(''));
-    }
-  }
-}
-
-// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-function operate(operatorFunc, num1, num2) {
-  num1 = parseFloat(num1);
-  num2 = parseFloat(num2);
-
-  switch (operatorFunc) {
-    case '*':
-      return multiply(num1, num2);
-    case '/':
-      if (num2 === 0) display.textContent === 'Not a number.';
-      else return divide(num1, num2);
+  switch (operator) {
+    case 'x':
+      previousNum *= currentNum;
+      convertNum();
+      break;
+    case '÷':
+      if (currentNum <= 0) {
+        previousNum = 'Error';
+        renderDisplay();
+        break;
+      }
+      previousNum /= currentNum;
+      convertNum();
+      break;
     case '+':
-      return add(num1, num2);
+      previousNum += currentNum;
+      convertNum();
+      break;
     case '-':
-      return subtract(num1, num2);
-    default:
-      undefined;
+      previousNum -= currentNum;
+      convertNum();
+      break;
   }
 }
 
-// Event listeners
-buttonNodeList.forEach(button =>
-  button.addEventListener('click', e => renderDisplay(e.target.value)),
-);
+function convertNum() {
+  previousNum = roundNum(previousNum);
+  previousNum = previousNum.toString();
+  renderDisplay();
+}
+
+function numberHandler(number) {
+  if (previousNum !== '' && currentNum !== '' && operator === '') {
+    previousNum = '';
+    currentDisplay.textContent = currentNum;
+  }
+  if (currentNum.length <= 9) {
+    currentNum += number;
+    currentDisplay.textContent = currentNum;
+  }
+}
+
+function operatorHandler(op) {
+  if (previousNum === '') {
+    previousNum = currentNum;
+    validateOperator(op);
+  } else if (currentNum === '') {
+    validateOperator(op);
+  } else {
+    operate();
+    operator = op;
+    currentDisplay.textContent = '';
+    previousDisplay.textContent = previousNum + ' ' + operator;
+  }
+}
+
+function validateOperator(op) {
+  operator = op;
+  previousDisplay.textContent = previousNum + ' ' + operator;
+  currentDisplay.textContent = '';
+  currentNum = '';
+}
+
+function roundNum(num) {
+  return Math.round(num * 100000) / 100000;
+}
+
+function addDecimal() {
+  if (!currentNum.includes('.')) {
+    currentNum += '.';
+    currentDisplay.textContent = currentNum;
+  }
+}
+
+function renderDisplay() {
+  if (previousNum.length <= 9) {
+    currentDisplay.textContent = previousNum;
+  } else {
+    currentDisplay.textContent = previousNum.slice(0, 9) + '...';
+  }
+  previousDisplay.textContent = '';
+  operator = '';
+  currentNum = '';
+}
+
+function clearCalc() {
+  currentNum = '';
+  previousNum = '';
+  operator = '';
+  currentDisplay.textContent = '0';
+  previousDisplay.textContent = '';
+}
+
+function deleteCurrentOperand() {
+  currentDisplay.textContent = '0';
+  currentNum = '';
+}
+
+function handleDelete() {
+  if (currentNum !== '') {
+    currentNum = currentNum.slice(0, -1);
+    currentDisplay.textContent = currentNum;
+    if (currentNum === '') {
+      currentDisplay.textContent = '0';
+    }
+  }
+  if (currentNum === '' && previousNum !== '' && operator === '') {
+    previousNum = previousNum.slice(0, -1);
+    currentDisplay.textContent = previousNum;
+  }
+}
+
+equals.addEventListener('click', () => {
+  if (currentNum != '' && previousNum != '') {
+    operate();
+  }
+});
+
+clear.addEventListener('click', clearCalc);
+numbers.forEach(btn => {
+  btn.addEventListener('click', e => {
+    numberHandler(e.target.textContent);
+  });
+});
+
+operators.forEach(btn => {
+  btn.addEventListener('click', e => {
+    operatorHandler(e.target.textContent);
+  });
+});
+
+decimal.addEventListener('click', () => {
+  addDecimal();
+});
+
+deleteCurrent.addEventListener('click', e => {
+  deleteCurrentOperand();
+});
